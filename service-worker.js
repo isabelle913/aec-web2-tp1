78de35cda24718d18d28a3e64171732a0cb39629
isabelle913 <atelierisabelle@videotron.ca>
2023-07-10 21:01:19 -0400

    Commenté les console.log

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,16 @@
-console.log("Coucou");
+console.log("fichier service-worker");
 
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = "static-cache-v1";
+const CACHE_NAME = "static-cache-v1.2";
 //Add list of files to cache here.
 const FILES_TO_CACHE = ["offline.html"];
 
 self.addEventListener("install", (evt) => {
-  console.log("[ServiceWorker] Install");
+  //console.log("[ServiceWorker] Install");
   // Precache static resources here.
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("[SW] Met en cache");
+      //console.log("[SW] Met en cache");
       return cache.addAll(FILES_TO_CACHE);
     })
   );
@@ -18,7 +18,7 @@ self.addEventListener("install", (evt) => {
 });
 
 self.addEventListener("activate", (evt) => {
-  console.log("[ServiceWorker] Activate");
+  //console.log("[ServiceWorker] Activate");
   //Remove previous cached data from disk.
 
   evt.waitUntil(
@@ -26,7 +26,7 @@ self.addEventListener("activate", (evt) => {
       return Promise.all(
         keyList.map((key) => {
           if (key !== CACHE_NAME) {
-            console.log("[ServiceWorker] Removing old cache", key);
+            //console.log("[ServiceWorker] Removing old cache", key);
             return caches.delete(key);
           }
         })
@@ -37,7 +37,7 @@ self.addEventListener("activate", (evt) => {
 });
 
 self.addEventListener("fetch", (evt) => {
-  console.log("[ServiceWorker] Fetch", evt.request.url);
+  //console.log("[ServiceWorker] Fetch", evt.request.url);
   //Add fetch event handler here.
   if (evt.request.mode !== "navigate") {
     // Not a page navigation, bail.
